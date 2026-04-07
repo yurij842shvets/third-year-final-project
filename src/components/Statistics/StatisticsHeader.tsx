b25eb18e96bb5e38049cfeb60d6e06add990d276
yurij842shvets <yurij2011shvets@gmail.com>
2026-04-07 19:27:05 +0300

    fixed exit account

--- src/components/Statistics/StatisticsHeader.tsx
+++ src/components/Statistics/StatisticsHeader.tsx
@@ -1,9 +1,20 @@
 import logo from "../../assets/logo.png";
 import userPic from "../../assets/user-pic.png";
 import { useAppSelector } from "../../redux/hooks/hooks";
+import { logout } from "../../redux/slice/authSlice";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../redux/hooks/hooks";
 
 export default function StatisticsHeader() {
   const userName = useAppSelector((state) => state.auth.currentUser?.name)
+  const navigate = useNavigate()
+  const dispatch = useAppDispatch()
+
+      const handleLogout = () => {
+        dispatch(logout())
+        navigate('/')
+      }
+
   return (
     <header className="main-page-header">
       <div className="logo">
@@ -14,7 +25,7 @@ export default function StatisticsHeader() {
       <div className="main-page-links-wrapper">
         <img src={userPic} alt="user" />
         <p className="main-page-link">{userName}</p>
-        <p className="main-page-link">Вийти</p>
+        <p className="main-page-link" onClick={handleLogout}>Вийти</p>
       </div>
     </header>
   );
